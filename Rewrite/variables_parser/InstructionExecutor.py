1ad601a840e5de51ad00e35671689146d2e71093
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-01-26 13:09:45 +0100

    recursion depth, typing check, better NotImplemented catch

--- Rewrite/variables_parser/InstructionExecutor.py
+++ Rewrite/variables_parser/InstructionExecutor.py
@@ -12,16 +12,24 @@ class ParsedExecutionFailure(CustomDiscordException):
     def __init__(self, instruction: Instruction, cause: Exception | None = None) -> None:
         super().__init__(f'Failed to execute Instruction of type **{instruction.type}** given options \'{instruction.options}\'', cause)
 
+class ParsedExecutionRecursionDepthLimit(CustomDiscordException):
+    def __init__(self, instructions: list[Instruction], depth: int) -> None:
+        super().__init__(f'Maximum recursion depth of {depth} exceeded maximal value when executing Instructions.\n'
+                         f'{"\n".join(str(i) for i in instructions)}')
 
+MAX_EXECUTION_RECUSION_DEPTH = 5 # todo: into config file you go.
 
 class InstructionExecutor:
     def __init__(self, client: BotClient):
         self.client = client
 
-    async def run(self, instructions: list[Instruction], interaction: discord.Interaction | discord.Message):
+    async def run(self, instructions: list[Instruction], interaction: discord.Interaction | discord.Message, depth: int = None, build: str = None):
         # TODO: make fail if not ran in guild context. Do not let DM context make things more complicated. Get this to work first.
+        depth: int = depth + 1 if depth else 0
+        if depth > MAX_EXECUTION_RECUSION_DEPTH:
+            raise ParsedExecutionRecursionDepthLimit(instructions, depth)
         i: int = 0
-        build: str = "" # expanded until finished or message sends, then reset.
+        build: str = build if build else "" # expanded until finished or message sends, then reset.
         mem: dict[str, ...] = {}
         while i < len(instructions):
             instruction = instructions[i]
@@ -33,11 +41,13 @@ class InstructionExecutor:
                     await self.send_output(build)
                     build = ""
                 elif instruction.type == InstructionType.DEFINE:
-                    mem[instruction.options['name']] = instruction.options['content']
+                    mem[str(instruction.options['name'])] = instruction.options['content']
                 elif instruction.type == InstructionType.SLEEP:
                     await self.sleep(instruction.options['content'])
                 else:
-                    build += '{ Skip exec of type ' + instruction.type + '; NotImplemented }'
+                    raise NotImplementedError()
+            except NotImplementedError:
+                build += '{ Skip exec of type ' + str(instruction.type) + '; NotImplemented }'
             except Exception as e:
                 raise ParsedExecutionFailure(instruction, cause=e)
             i += 1
@@ -50,6 +60,9 @@ class InstructionExecutor:
 
     async def sleep(self, time: int | float):
         await _asyncio.sleep(time)
+
+    def basic_replace(self, interaction: discord.Interaction | discord.Message, key: str) -> str:
+        raise NotImplementedError()
 
 class DebugInstructionExecutor(InstructionExecutor):
     def __init__(self, client: BotClient):
