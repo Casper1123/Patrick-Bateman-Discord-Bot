d354bad159a5b32d2c7b8753049691fc11067318
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-01-27 13:18:54 +0100

    slowly implementing being able to send messages

--- Rewrite/variables_parser/InstructionExecutor.py
+++ Rewrite/variables_parser/InstructionExecutor.py
@@ -9,8 +9,8 @@ from . import Instruction, InstructionType
 
 
 class ParsedExecutionFailure(CustomDiscordException):
-    def __init__(self, instruction: Instruction, cause: Exception | None = None) -> None:
-        super().__init__(f'Failed to execute Instruction of type **{instruction.type}** given options \'{instruction.options}\'', cause)
+    def __init__(self, instruction: Instruction, index: int, cause: Exception | None = None) -> None:
+        super().__init__(f'Failed to execute Instruction of type **{instruction.type}** (index {index}) given options \'{instruction.options}\'', cause)
 
 class ParsedExecutionRecursionDepthLimit(CustomDiscordException):
     def __init__(self, instructions: list[Instruction], depth: int) -> None:
@@ -23,11 +23,12 @@ class InstructionExecutor:
     def __init__(self, client: BotClient):
         self.client = client
 
-    async def run(self, instructions: list[Instruction], interaction: discord.Interaction | discord.Message, depth: int = None, build: str = None):
+    async def run(self, instructions: list[Instruction], interaction: discord.Interaction | discord.Message, depth: int = None, build: str = None, push_final_build: bool = True, fresh: bool = True) -> tuple(str | None, bool):
         # TODO: make fail if not ran in guild context. Do not let DM context make things more complicated. Get this to work first.
         depth: int = depth + 1 if depth else 0
         if depth > MAX_EXECUTION_RECUSION_DEPTH:
             raise ParsedExecutionRecursionDepthLimit(instructions, depth)
+        first_reply = fresh
         i: int = 0
         build: str = build if build else "" # expanded until finished or message sends, then reset.
         mem: dict[str, ...] = {}
@@ -38,27 +39,50 @@ class InstructionExecutor:
                     build += instruction.options['content']
                 elif instruction.type == InstructionType.PUSH:
                     # todo: settings for message. Assuming it's sent right now.
-                    await self.send_output(build)
+                    if build == "": raise ValueError('Instruction of type PUSH did not receive content to push.')
+                    await self.send_output(build, interaction, first_reply=first_reply)
                     build = ""
+                    first_reply = False
                 elif instruction.type == InstructionType.DEFINE:
                     mem[str(instruction.options['name'])] = instruction.options['value']
                 elif instruction.type == InstructionType.SLEEP:
                     await self.sleep(instruction.options['time'])
                 elif instruction.type == InstructionType.BASIC_REPLACE:
                     build += self.basic_replace(interaction, instruction.options['key'])
+                elif instruction.type == InstructionType.WRITING:
+                    build, first_reply = await self.is_writing(instruction.options['instructions'], interaction, depth, build)
+                    if build is None:
+                        raise TypeError('Instruction of type WRITING returned None value instead of String.')
                 else:
                     raise NotImplementedError()
             except NotImplementedError:
                 build += '{ Skip exec of type ' + str(instruction.type) + '; NotImplemented }'
             except Exception as e:
-                raise ParsedExecutionFailure(instruction, cause=e)
+                raise ParsedExecutionFailure(instruction, i, cause=e)
             i += 1
-        if build:
-            await self.send_output(build)
 
-    async def send_output(self, out: str):
+        if build and push_final_build:
+            await self.send_output(build, interaction, first_reply=first_reply)
+            return None, first_reply
+        else:
+            return build, first_reply
+
+    async def send_output(self, out: str, interaction: discord.Interaction | discord.Message, first_reply: bool = True) -> None:
         ...  # todo: implement sending data.
-        if out == "": out = "{ ? Empty output string ? }"  # cannot send empty data. Substituting to allow for debugging.
+        if isinstance(interaction, discord.Message):
+            ... # case Message
+            # reply
+            interaction.reply(out, delete_after=..., mention_author=..., allowed_mentions=...)
+            # not reply
+            interaction.channel.send(out, delete_after=..., mention_author=..., allowed_mentions=...)
+            # Allowed mentions notes, see contructor:
+            discord.AllowedMentions(everyone= False, users = False, roles = False, replied_user=False) # users & roles can be collections as well.
+            discord.AllowedMentions.all() # also an option
+        else:
+            ... # case Interaction
+            interaction.edit_original_response(out, allowed_mentions=...)
+
+
 
     async def sleep(self, time: int | float):
         await _asyncio.sleep(time)
@@ -66,12 +90,26 @@ class InstructionExecutor:
     def basic_replace(self, interaction: discord.Interaction | discord.Message, key: str) -> str:
         raise NotImplementedError()
 
+    async def is_writing(self, instructions: list[Instruction], interaction: discord.Interaction | discord.Message, depth: int, build: str) -> tuple[str | None, bool]:
+        async with interaction.channel.typing():
+            return await self.run(instructions, interaction, depth, build, False)
+
 class DebugInstructionExecutor(InstructionExecutor):
     def __init__(self, client: BotClient):
         super().__init__(client)
 
-    async def send_output(self, out: str):
+    async def send_output(self, out: str, interaction: discord.Interaction | discord.Message, first_reply: bool = True):
         raise NotImplementedError()
 
     async def sleep(self, time: int | float):
         raise NotImplementedError()
+
+    async def basic_replace(self, interaction: discord.Interaction | discord.Message, key: str) -> str:
+        raise NotImplementedError()
+
+    async def is_writing(self, instructions: list[Instruction], interaction: discord.Interaction | discord.Message, depth: int, build: str) -> tuple[str | None, bool]:
+        build += '{ As writing; Start }'
+        build_out, first_reply = await self.run(instructions, interaction, depth, build, False)
+        build += build_out if build_out else ''
+        build += '{ As writing; End }'
+        return build, first_reply
