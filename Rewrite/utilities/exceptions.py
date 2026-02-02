af979c9f4bac89cd9eae3816c332f317b5145d08
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-02-02 14:13:16 +0100

    Updating framework to allow passing just a cause as well.

--- Rewrite/utilities/exceptions.py
+++ Rewrite/utilities/exceptions.py
@@ -15,9 +15,9 @@ def exception_as_embed(err: Exception) -> Embed:
 
 class CustomDiscordException(Exception):
     """
-    One of my many wild mostly useless ideas. Explore later maybe?
+    Template exception which includes simple visualisation for user-feedback.
     """
-    def __init__(self, message: str, cause: Exception | None = None, error_type: str | None = None) -> None:
+    def __init__(self, message: str = None, cause: Exception | None = None, error_type: str | None = None) -> None:
         self.error_type = error_type if error_type else type(self).__name__  # Should work through inheritance, right?
         self.message = message
         self.cause = cause
@@ -28,9 +28,10 @@ class CustomDiscordException(Exception):
             title=self.error_type,
             description=f"**An error has occurred.**\n"
                         f"If this issue persists, feel free to report it on [the development's issues page](<https://github.com/Casper1123/Patrick-Bateman-Discord-Bot/issues>).\n"
-                        f"\n"
-                        f"**Error:**\n"
-                        f"{self.message}"
+                        f"{f'\n**Error:**\n{self.message}' if self.message else ''}"
                         f"{'\n\n**Caused by:**' + type(self.cause).__name__ + '\n' + str(self.cause) if self.cause else ''}",
         )
         return embed
+
+    def __str__(self) -> str:
+        return f'{self.error_type}: {self.message} caused by {self.cause}'
