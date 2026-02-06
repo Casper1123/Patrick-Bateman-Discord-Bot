974e9186efe1736fbb9494b0b46f43ca2008f482
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-02-06 18:16:23 +0100

    rename subopts in example line

--- Rewrite/testing.py
+++ Rewrite/testing.py
@@ -6,7 +6,7 @@ import asyncio
 fact1: str = 'This is a fact with {guild.id} in it as well as a writing block.{writing(sleep(); push(1))}Isn\'t it awesome?!'
 fact2: str = 'opt {choice(\'First Option\', \'Second Option\', \'Third option: {sleep(); self}\')}'
 fact3: str = 'This fact has a random user in it: {tru(0)}'
-fact4: str = '{choice(\'C1\', \'C2{choice("Fuck1", "Fuck2{choice(\'Opt1\', \'{sleep(); writing(push(1); self)}C2\')}")}\')}'
+fact4: str = '{choice(\'C1\', \'C2{choice("subopt1", "subopt2{choice(\'Opt1\', \'{sleep(); writing(push(1); self)}subopt2\')}")}\')}'
 output: list[Instruction] = parse_variables(fact4)
 for i in output:
     print(i)
