55f8a41b097d68f7f0cdd94eb15a254882ed5dad
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-02-06 12:17:51 +0100

    More fact tests

--- Rewrite/testing.py
+++ Rewrite/testing.py
@@ -3,9 +3,10 @@ from Rewrite.variables_parser.instructionexecutor import *
 
 import asyncio
 
-fact: str = 'This is a fact with {guild.id} in it as well as a writing block.{writing(sleep(); push())}Isn\'t it awesome?!'
-_fact: str = 'This fact has two options: {choice(\'First Option\', \'Second Option\')}'
-output: list[Instruction] = parse_variables(fact)
+fact1: str = 'This is a fact with {guild.id} in it as well as a writing block.{writing(sleep(); push())}Isn\'t it awesome?!'
+fact2: str = 'This fact has two options: {choice(\'First Option\', \'Second Option\')}'
+fact3: str = 'This fact has a random user in it: {tru(0, name)}'
+output: list[Instruction] = parse_variables(fact3)
 for i in output:
     print(i)
 
