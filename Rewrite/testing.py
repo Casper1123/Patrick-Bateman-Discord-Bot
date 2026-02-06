a964736cd3a23abd5217413803569bef8f02eeca
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-02-06 13:03:20 +0100

    Refined tru regex for optional attribute

--- Rewrite/testing.py
+++ Rewrite/testing.py
@@ -5,7 +5,7 @@ import asyncio
 
 fact1: str = 'This is a fact with {guild.id} in it as well as a writing block.{writing(sleep(); push())}Isn\'t it awesome?!'
 fact2: str = 'This fact has two options: {choice(\'First Option\', \'Second Option\')}'
-fact3: str = 'This fact has a random user in it: {tru(0, name)}'
+fact3: str = 'This fact has a random user in it: {tru(0)}'
 output: list[Instruction] = parse_variables(fact3)
 for i in output:
     print(i)
