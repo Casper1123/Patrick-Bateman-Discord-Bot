8b91e2e1b74a00222a6530dad475f2d664a1224a
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-02-09 16:19:08 +0100

    Fact input testing helper function

---
+++ Rewrite/variables_parser/testing.py
@@ -0,0 +1,23 @@
+from discord import Interaction, Message, Embed
+
+from Rewrite.discorduser import BotClient
+from Rewrite.variables_parser import Instruction, parse_variables, InstructionParseError
+from Rewrite.variables_parser.instructionexecutor import DebugInstructionExecutor, ParsedExecutionFailure
+
+
+async def test_raw_input(client: BotClient, interaction: Interaction | Message, text: str, ephemeral: bool) -> bool:
+    """
+    Compiles and test executes given PISS input.
+    If unsuccessful, automatically sends information Embed message, assuming it hasn't had any messages sent yet.
+    :return: Success.
+    """
+    try:
+        compiled: list[Instruction] = parse_variables(text)
+        executor: DebugInstructionExecutor = DebugInstructionExecutor(client)
+        await executor.run(compiled, interaction)
+    except InstructionParseError or ParsedExecutionFailure as e:
+        await interaction.send_message(ephemeral=ephemeral, embed=Embed(
+            title=f'Input failed to {'compile' if isinstance(e, InstructionParseError) else 'test execution'}.',
+            description='Aborting operation. Please properly test your fact using `/admin preview`.'))
+        return False
+    return True
