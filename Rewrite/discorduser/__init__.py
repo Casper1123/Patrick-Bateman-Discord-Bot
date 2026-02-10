ee6c9b543566fd6021eed4989e3724e5a7faa9f7
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-02-10 22:55:52 +0100

    refining selective exception logging

--- Rewrite/discorduser/__init__.py
+++ Rewrite/discorduser/__init__.py
@@ -3,6 +3,7 @@ import socket
 
 import discord
 from discord import app_commands
+from discord.app_commands import CommandOnCooldown
 from discord.ext import commands
 
 from Rewrite.data.data_interface_abstracts import DataInterface
@@ -10,7 +11,7 @@ from Rewrite.discorduser.logger.logger import Logger
 from Rewrite.utilities.exceptions import CustomDiscordException
 from Rewrite.variables_parser import InstructionParseError
 
-UNLOGGED_EXCEPTION_TYPES = [InstructionParseError.__name__] # using __name__ to ensure that when I change the class names this updates.
+UNLOGGED_EXCEPTION_TYPES = [InstructionParseError.__name__, CommandOnCooldown.__name__] # using __name__ to ensure that when I change the class names this updates.
 
 
 class BotClient(commands.Bot):
@@ -46,15 +47,21 @@ class BotClient(commands.Bot):
                     return # Skip 'connection lost' exceptions, also removing them from the logging.
                     # Idk why, but for some reason my host device seems to lose connection at unknown intervals for short periods of time.
                     # So this is temporary glue fix.
-                if not isinstance(error, CustomDiscordException):
+                log: bool = True
+                if isinstance(error, CommandOnCooldown):
+                    log = type(error).__name__ not in UNLOGGED_EXCEPTION_TYPES
+                    error = CustomDiscordException(message=str(error), error_type=error.__name__)
+                elif not isinstance(error, CustomDiscordException):
+                    log = type(error).__name__ not in UNLOGGED_EXCEPTION_TYPES
                     error_old = error
                     error: CustomDiscordException = CustomDiscordException(cause=error_old, error_type=type(error).__name__)
+                else:
+                    assert isinstance(error, CustomDiscordException)
+                    log = type(error.cause).__name__ not in UNLOGGED_EXCEPTION_TYPES
 
                 await interaction.edit_original_response(embed=error.as_embed())  # Can get more detailed information from this.
-                if not type(error) in UNLOGGED_EXCEPTION_TYPES: # fixme: this setup is ASS.
-                    if ((isinstance(error, CustomDiscordException) and error.cause.__name__ not in UNLOGGED_EXCEPTION_TYPES) or
-                            (not isinstance(error, CustomDiscordException) and error.__name__ not in UNLOGGED_EXCEPTION_TYPES)):
-                        await self.logger.log_error(error, interaction)
+                if log:
+                    await self.logger.log_error(error, interaction)
                 raise error
 
 
