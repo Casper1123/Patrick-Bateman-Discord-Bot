49ae51b292b515919bd51d1fbb55a06d3a9bf5f1
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-02-11 10:57:21 +0100

    fact cooldown

--- Rewrite/discorduser/cogs/facts.py
+++ Rewrite/discorduser/cogs/facts.py
@@ -7,6 +7,8 @@ from ...data.data_interface_abstracts import DataInterface
 from ...variables_parser import parse_variables, Instruction
 from ...variables_parser.instructionexecutor import InstructionExecutor
 
+FACT_COOLDOWN: float = 1.0
+
 
 class FactsCog(commands.Cog):
     def __init__(self, client: BotClient,  db: DataInterface) -> None:
@@ -15,6 +17,7 @@ class FactsCog(commands.Cog):
 
     @app_commands.command(name="fact", description="Gives a fact.")
     @app_commands.describe(index="The index of the fact you would like to request.")
+    @app_commands.checks.cooldown(1, FACT_COOLDOWN, key=lambda i: (i.guild_id, i.user.id))
     async def fact_give(self, interaction: discord.Interaction, index: int = None, ephemeral: bool = False):
         try:
             fact_raw: str = self.db.get_fact(interaction.guild_id if not self.client.local_fact_kill_switch else None, index)
@@ -28,6 +31,9 @@ class FactsCog(commands.Cog):
 
 
     @app_commands.command(name="fact_index", description="Gives the number of stored facts.")
+    @app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+                # hardcoded 10s because this command is not as useful
+                # and I'd like to save on DB cals
     async def fact_index(self, interaction: discord.Interaction):
         global_fact_count: int = self.db.get_fact_count(None)
         local_fact_count: int = self.db.get_fact_count(interaction.guild_id) if not self.client.local_fact_kill_switch else 0
