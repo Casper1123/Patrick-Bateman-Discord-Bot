961572d5c17cf4eb4dab3f72aba328cef04a66c5
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-02-13 17:47:16 +0100

    tables design data implementation

--- Rewrite/data/README.md
+++ Rewrite/data/README.md
@@ -3,13 +3,3 @@ Store global, local, and configuration data if possible.
 Work out properly. Make sure to guarantee not leaking stuff.
 
 Also like, don't forget about making a tool that translates previously added data into new db okay.
-
-
-
-
-# Facts:
-Each entry has:
-- AuthorID (keep track of last modified user ID)
-- Text
-- GuildID
-- ModifiedAt (UNIX Timestamp) (to order for indices)
--- Rewrite/data/implementation.py
+++ Rewrite/data/implementation.py
@@ -4,8 +4,20 @@ from Rewrite.data.data_interface_abstracts import GlobalAdminDataInterface, Fact
 class SQLDataBase(GlobalAdminDataInterface):
     def __init__(self):
         pass
+        # todo: do not forget to implement caching!
 
     # region facts
+    """
+    Table(s) and design:
+    # LocalFacts:
+    - int; AuthorID (keep track of last modified user ID)
+    - str; Text
+    - int; GuildID
+    - int; ModifiedAt (UNIX Timestamp) (to order for indices)
+    PK: (GuildID, AuthorID, Text)
+    Order by ModifiedAt for Indexing purposes.
+    Disallows users adding duplicate facts.
+    """
     # region DataInterface
     def get_fact(self, guild_id: int | None, index: int | None) -> str:
         raise NotImplementedError()
@@ -33,6 +45,16 @@ class SQLDataBase(GlobalAdminDataInterface):
     # endregion
     # endregion
     # region global facts
+    """
+    Table(s) and design:
+    # GlobalFacts:
+    - int; AuthorID (keep track of last modified user ID)
+    - str; Text
+    - int; ModifiedAt (UNIX Timestamp) (to order for indices)
+    PK: (AuthorID, Text)
+    Order by ModifiedAt for Indexing purposes.
+    Disallows users adding duplicate facts.
+    """
     def create_global_fact(self, user_id: int, fact: str) -> bool:
         raise NotImplementedError()
 
@@ -46,6 +68,22 @@ class SQLDataBase(GlobalAdminDataInterface):
         raise NotImplementedError()
     # endregion
     # region moderation
+    """
+    Table(s) and design:
+    # BannedUsers
+    - int; UserID
+    PK: (UserID)
+    
+    # BannedGuilds
+    - int; GuildID
+    PK: (GuildID)
+    
+    # SuperServers
+    - int; GuildID
+    PK: (GuildID)
+    
+    All three simple lists of ids to keep track of.
+    """
     # region LocalAdminDataInterface
     def is_banned_user(self, user_id: int) -> bool:
         raise NotImplementedError()
@@ -75,6 +113,13 @@ class SQLDataBase(GlobalAdminDataInterface):
     # endregion
 
     # region other
+    """
+    Table(s) and design:
+    # LocalLogChannels
+    - int; GuildID
+    - int; ChannelID
+    PK: (GuildID)
+    """
     # region LocalAdminDataInterface
     def set_log_output(self, guild_id: int, channel_id: int | None) -> None:
         raise NotImplementedError()
