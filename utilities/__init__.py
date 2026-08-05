4edf4bbd4227a44c2a8364dc874579d4dcda63b6
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-08-05 20:07:40 +0200

    fixing crude-ish json utils

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -1,11 +1,11 @@
 import json as _json
 
-def load_json(filename: str) -> dict | list:
-    with open(f"json_files/{filename}", "r") as cj:
+def load_json(filepath: str) -> dict | list:
+    with open(filepath, "r") as cj:
         return _json.load(cj)
 
 
 def write_json(filepath: str, cj_dict: dict | list[str | float | int], sort_keys: bool = False, indent: int | None = None):
-    with open(f"{filepath}", "w") as cj:
-        _json.dump(cj_dict, cj, sort_keys=sort_keys, indent=indent)
+    with open(filepath, "w") as cj:
+        _json.dump(cj_dict, cj, sort_keys=sort_keys, indent=indent) # noqa
 
