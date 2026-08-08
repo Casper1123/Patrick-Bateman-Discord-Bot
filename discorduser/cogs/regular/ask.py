a7ba214ab52c9d37305b4ec3f417a12618b26b2a
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-08-09 01:46:41 +0200

    commented out some temp work on ask. CBA right now

--- discorduser/cogs/regular/ask.py
+++ discorduser/cogs/regular/ask.py
@@ -1,7 +1,8 @@
 import asyncio
 import random
 
-from discord import app_commands, Interaction, Message
+from discord import app_commands, Interaction, Message, HTTPException, NotFound
+from discord.app_commands import GuildAppCommandPermissions, AppCommand
 from discord.ext import commands
 
 from data.interfaces.saying import SayingInterface
@@ -9,6 +10,7 @@ from discorduser.user.abstract import BotClient
 from piss import Instruction, parse_variables
 from piss.instructionexecutor import InstructionExecutor
 
+_ask_command_name: str = 'ask'
 
 @app_commands.guild_only()
 class AskPatrick(commands.Cog):
@@ -16,20 +18,38 @@ class AskPatrick(commands.Cog):
         self.client = client
         self.saying = saying
 
+    @app_commands.command(name=_ask_command_name, description="A command-type shortcut to 'ask @botname <question>'.")
+    @app_commands.describe(question="The question to ask.")
+    async def ask_patrick_command(self, interaction: Interaction, question: str):
+        await self.ask_patrick(interaction, question)
+
     @commands.Cog.listener("on_message")
     async def ask_patrick_listener(self, message: Message):
         if not message.content.lower().startswith(f"ask <@{self.client.user.id}>"):
             return
+        """
+        try:
+            # todo: this is an ABSOLUTE MESS to implement. Procrastinating on it.
+            # todo: move to cached helper function because this is expensive as hell.
+            cmds: list[AppCommand] = await self.client.tree.fetch_commands(guild=message.guild)
+            command = next(
+                (cmd for cmd in cmds if cmd.name == _ask_command_name),
+                None,
+            )
+            if not command: return
+            permissions: GuildAppCommandPermissions = await command.fetch_permissions(message.guild)
+        except (HTTPException, NotFound): # noqa
+            return
+
+        # How to decide here?
+        """
+
+
         # todo: check if command is callable here by user. If not, back out.
         split_content = message.content.split()
         if len(split_content) < 3:
             return  # Ignore if no question asked.
         await self.ask_patrick(message, " ".join(split_content[3:]))
-
-    @app_commands.command(name="ask", description="A command-type shortcut to 'ask @botname <question>'.")
-    @app_commands.describe(question="The question to ask.")
-    async def ask_patrick_command(self, interaction: Interaction, question: str):
-        await self.ask_patrick(interaction, question)
 
     async def ask_patrick(self, message: Message | Interaction, question: str):
         async def ask_reply(replyable: Message | Interaction, content: str,
