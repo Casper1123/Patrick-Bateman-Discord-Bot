e397a30053bc7d8f2fe9aefa8e4e181a256182eb
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-08-19 15:27:16 +0200

    need more progress porting Push

--- piss/instructions/push.py
+++ piss/instructions/push.py
@@ -1,14 +1,10 @@
 from enum import Enum as _Enum
 from re import Match as _Match
 
+from discord import AllowedMentions as _AllowedMentions
+
 from piss.instructions.abstract import Instruction as _Instruction
 from piss.exceptions import InstructionParseError as _InstructionParseError
-
-
-class MentionOptions(_Enum):
-    NONE = 0
-    AUTHOR = 1
-    ALL = 2
 
 
 class PushInstruction(_Instruction):
@@ -31,7 +27,7 @@ class PushInstruction(_Instruction):
         except ValueError:
             raise _InstructionParseError(match.group(0), f'Could not parse {pingable_val} into an Integer.')
 
-        if not pingable_val in [0, 1, 2]:
+        if not pingable_val in {0, 1, 2}:
             raise _InstructionParseError(match.group(0), f'Pingable option **{pingable_val}** not in **[0, 1, 2]**.')
 
         if pingable_val == 2:
@@ -43,5 +39,11 @@ class PushInstruction(_Instruction):
 
         return PushInstruction(pingable)
 
-    def __init__(self, pingable: MentionOptions = MentionOptions.NONE) -> None:
+    def __init__(self, pingable: _AllowedMentions | None = None) -> None:
+        if not pingable:
+            # Everyone: @everyone & @here
+            # Users: Can ping other users
+            # Roles: Can ping roles
+            # Replied_user: If to ping the author if replying.
+            pingable = _AllowedMentions(everyone=False, users=False, roles=False, replied_user=False)
         self.pingable = pingable
