f1eb34fb2f6731d45bbacd6624bfd1eca5678abc
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-08-22 17:20:23 +0200

    makeshift implementation Choice parser
    Mind the circular import. Figure that out later.

--- piss/instructions/choice.py
+++ piss/instructions/choice.py
@@ -1,16 +1,16 @@
 from re import Match as _Match
+import ast as _ast
 
 from piss.instructions.abstract import Instruction as _Instruction
-from piss.exceptions import InstructionParseError as _InstructionParseError
-from utilities.exceptions import ErrorTooltip
-from piss.instructions import _doubles, _bounds, _escapes, _be_map
+# noinspection PyProtectedMember
+from piss.parsing import _parse_top_level # todo fixme: circular import!
 
 
 option_bounds: list[str] = ["'", '"']  # ' "
 
 class ChoiceInstruction(_Instruction):
-    def __init__(self, options: tuple[list[_Instruction], ...]):
-        self.options: tuple[list[_Instruction], ...] = options
+    def __init__(self, options: list[list[_Instruction]]):
+        self.options: list[list[_Instruction]] = options
 
     def __str__(self) -> str:
         return super().__str__() + f'[opt={self.options}]'
@@ -22,14 +22,29 @@ class ChoiceInstruction(_Instruction):
     @staticmethod
     def from_match(match: _Match, ident: int, memory_stack: list[dict[str, type]], recursion_depth: int = 0,
                    writing: bool = False) -> ChoiceInstruction:
+                   writing: bool) -> ChoiceInstruction:
         if not ident == 0:
             raise ValueError('Unsupported match identifier for Instruction of type Choice')
 
         # todo: remake entirely.
         # Tldr; choice(str, str*, str) where str* is any number >= 0 str input, each bounded with either ' or "
         # Need to individually parse the choices given the parsing function _parse_top_level
+        # _ast.literal_eval should perform this function, but double check if that is true through testing.
+        opt_raw = _ast.literal_eval(f'({match.group('options')})')
 
+        if not isinstance(opt_raw, tuple):
+            raise ValueError('Match not parsed as tuple. Probably a non-user error.')
 
+        for x in opt_raw:
+            if not isinstance(x, str):
+                raise TypeError(f'Options entry of type {type(x).__name__}, not string for option {x}')
 
+        opt_raw: tuple[str, ...]
 
-        ...
+        # Turn into Instructions
+        options: list[list[_Instruction]] = []
+        for x in opt_raw:
+            instr: list[_Instruction] = _parse_top_level(x, recursion_depth + 1, memory_stack, writing)
+            options.append(instr)
+
+        return ChoiceInstruction(options)
