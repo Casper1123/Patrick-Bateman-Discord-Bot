eadcd97ce6c3a23f79fbb1547576b10a87d97d3a
Casper1123 <42973822+Casper1123@users.noreply.github.com>
2026-08-22 17:30:24 +0200

    Update parse_order.py

--- piss/parsing/parse_order.py
+++ piss/parsing/parse_order.py
@@ -1,6 +1,17 @@
 from piss.instructions.abstract import Instruction
+from piss.instructions.choice import ChoiceInstruction
 from piss.instructions.push import PushInstruction
+from piss.instructions.randnum import RandomNumberInstruction
+from piss.instructions.randuser import RandomUserInstruction
+from piss.instructions.sleep import SleepInstruction
+from piss.instructions.writing import WritingInstruction
 
 parse_order: tuple[type[Instruction], ...] = (
-    PushInstruction, # todo: populate
+    PushInstruction,
+    RandomNumberInstruction,
+    RandomUserInstruction,
+    SleepInstruction,
+    WritingInstruction,
+    # todo: Causes circular import! Order -> ChoiceInstruction -> parser -> order
+    ChoiceInstruction,
 )
